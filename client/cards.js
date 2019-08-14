fa304b2e16f36ae9e58a977f52a81476db4a1575
tonykjin <tonyjin23@gmail.com>
2019-08-14 00:06:33 +0000

    card back now showing, fix both front/back divs showing

--- client/cards.js
+++ client/cards.js
@@ -1,10 +1,10 @@
 class Cards{
     renderCards = ( cardsArr ) => {
         this.card = $('<div></div>').addClass('card');
-        this.cardBack = $('<div></div>').addClass('cardBack');
-        this.cardFront = $('<div></div>').addClass('cardFront');
+        this.cardBack = $('<div></div>').addClass('col').attr('id', 'cardBack');
+        this.cardFront = $('<div></div>').addClass('col').attr('id', 'cardFront');
         $(".row").append(this.card);
         $(this.card).append(this.cardFront);
-        $(this.card).append(this.cardBack);
+        $(this.card).prepend(this.cardBack);
     }
 }
