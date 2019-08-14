906251551981d3618ce2853876ce8e0b5f8cea77
tonykjin <tonyjin23@gmail.com>
2019-08-14 22:31:38 +0000

    created shuffle function

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ $(document).ready(init);
 
 function init() {
     let cards = new Cards;
-    cards.renderCards();
+    let cardsArr = ['drunk_rick.jpg','drunk_rick.jpg', 'neutrino_bomb.png', 'neutrino_bomb.png', 'summer.jpg', 'summer.jpg'];
+    cards.renderCards(cards.shuffle([...cardsArr]));
 }
     
