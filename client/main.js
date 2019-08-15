8d39b14c91550a68e04cdb62e3f94ee2ca167a04
tonykjin <tonyjin23@gmail.com>
2019-08-15 20:25:43 +0000

    able to match cards and test if child node image matches

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@ function init() {
     let cards = new Cards;
     let cardsArr = ['drunk_rick.jpg','drunk_rick.jpg', 'neutrino_bomb.png', 'neutrino_bomb.png', 'summer.jpg', 'summer.jpg'];
     cards.renderCards(cards.shuffle([...cardsArr]));
-    cards.applyClickHandlers();
+    let logic = new Logic;
+    logic.applyClickHandlers();
 }
     
