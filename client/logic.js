eebdffc929a0f8ecc6ec5e5adb3647d00e0493e6
tonykjin <tonyjin23@gmail.com>
2019-08-16 23:41:29 +0000

    completed win modal

--- client/logic.js
+++ client/logic.js
@@ -9,7 +9,7 @@ class Logic {
         this.accuracy = 0;
         this.matchedCards = [];
         this.notClickable = false;
-        this.maxMatches = 8;
+        this.maxMatches = 9;
     }
     applyClickHandlers = () => {
         $('.card-inner').on('click', this.handleClick);
@@ -48,10 +48,8 @@ class Logic {
             this.matchedCards.push(this.firstCardClicked, this.secondCardClicked);
             this.removeMatchedClickHandlers(this.matchedCards);
             this.applyClickHandlers();
-            console.log(this.firstCardClicked);
-            console.log(this.secondCardClicked);
-            console.log(this.matchedCards);
             this.notClickable = false;
+            console.log(this.matches);
         } else {
             setTimeout(() => {
                 $(this.firstCardClicked).toggleClass('flipped');
@@ -59,39 +57,16 @@ class Logic {
                 this.notClickable = false;
             }, 1500);
             this.applyClickHandlers();
-            console.log('no match');
         }
         this.accuracy = this.calcAccuracy();
         if (this.matches === this.maxMatches) {
-            this.winCondition();
+            console.log(this.matchedCards);
+            $('#win-modal').modal('show');
         }
     }
     calcAccuracy = () => {
         return (
             ((this.matches)/(this.attempts)) * 100
-        );
-    }
-    winCondition = () => {
-        return (
-        `<div class="modal fade" id="win-modal" tabindex="-1" role="dialog" aria-labelledby="win-modal" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">Congratulations! You have saved dimension c-137</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              [Put some gifs here]
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary"> Play Again </button>
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
-          </div>
-        </div>
-      </div>`
         );
     }
  }
